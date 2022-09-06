06f14cae3252a642458e7ab0588d081049d31e1f
liyima <77553148+liyima@users.noreply.github.com>
2022-09-06 13:13:27 -0400

    Update copying_list1_to_list2.s
    indented to make the code more readable

--- copying_list1_to_list2.s
+++ copying_list1_to_list2.s
@@ -5,10 +5,10 @@
 .org 0x0000
 _start:
 
-	movi	sp,	0x7FFC
+	movi	sp, 0x7FFC
 	movia	r2, LIST1 # need movia to get the entire list, not just the first value
 	movia	r3, LIST2
-	ldw		r4, N(r0)
+	ldw	r4, N(r0)
 	
 	call ListCopy
 	
@@ -18,10 +18,10 @@ _end: # be able to call end anywhere
 ListCopy:
 	
 	subi	sp, sp, 16
-	stw		r2, 12(sp) # input list
-	stw		r3, 8(sp) # output list
-	stw		r4, 4(sp) # counter
-	stw		r5, 0(sp) # place to load and store for output list
+	stw	r2, 12(sp) # input list
+	stw	r3, 8(sp) # output list
+	stw	r4, 4(sp) # counter
+	stw	r5, 0(sp) # place to load and store for output list
 	
 LC_loop:
 
@@ -36,10 +36,10 @@ LC_loop:
 	# if counter is >0, loop through list
 	bgt r4, r0, LC_loop
 	
-	ldw		r2, 12(sp) # input list
-	ldw		r3, 8(sp) # output list
-	ldw		r4, 4(sp) # counter
-	ldw		r5, 0(sp) # place to load and store for output list
+	ldw	r2, 12(sp) # input list
+	ldw	r3, 8(sp) # output list
+	ldw	r4, 4(sp) # counter
+	ldw	r5, 0(sp) # place to load and store for output list
 	addi	sp, sp, 16
 	
 	ret
@@ -47,4 +47,4 @@ LC_loop:
 .org 0x1000
 LIST1:	.word 4, 6, 8, 9
 LIST2:	.skip 16
-N:		.word 4
+N:	.word 4
