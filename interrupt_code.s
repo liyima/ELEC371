cfdde3e943ca161cdc9cf6a946cafe45aa0d9c79
liyima <77553148+liyima@users.noreply.github.com>
2022-10-04 10:57:03 -0400

    Update interrupt_code.s

--- interrupt_code.s
+++ interrupt_code.s
@@ -1,4 +1,5 @@
 # assume in memory, FLAG, DATA_FROM_PORT (16 bits use movi, and 32 bits use movia)
 
 # interrupt code
-check:
+check_for_input_port_interrupt:
+  andi r3, r2, Ox2000 # assume ipending value is previously read into rz
